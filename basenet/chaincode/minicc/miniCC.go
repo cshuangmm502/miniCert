c93f8e2338fdef13ff85bf2132ba5cc2f46c5d19
cshuangmm502 <751579076@qq.com>
2021-08-13 11:59:39 +0800

    202108131159

--- basenet/chaincode/minicc/miniCC.go
+++ basenet/chaincode/minicc/miniCC.go
@@ -1,60 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-protos-go/peer"
-	"math/big"
 )
-
-func create_all_membership_witness(A0 *big.Int,set map[string]int,N *big.Int)[]*big.Int{
-	var primes []*big.Int
-	for k,v := range set{
-		prime := HashToPrimeWithNonce(k,v)
-		primes=append(primes, prime)
-		fmt.Println(k,prime)
-	}
-
-	fmt.Println(primes)
-	return root_factor(A0,primes,N)
-}
-
-func root_factor(g *big.Int,primes []*big.Int,N *big.Int)[]*big.Int{
-	n := len(primes)
-	if n==1{
-		var result []*big.Int
-		result = append(result,g)
-		return result
-	}
-
-	n_tag := n/2
-
-	primes_L := primes[n_tag:n]
-	product_L := calculate_product(primes_L)
-	g_L := big.NewInt(1)
-	g_L.Exp(g,product_L,N)
-
-	primes_R := primes[0:n_tag]
-	product_R := calculate_product(primes_R)
-	g_R := big.NewInt(1)
-	g_R.Exp(g,product_R,N)
-
-	L := root_factor(g_L, primes_R,N)
-	R := root_factor(g_R, primes_L,N)
-
-	var result []*big.Int
-	result = append(result, L...)
-	result = append(result, R...)
-	return result
-}
-
-func calculate_product(list []*big.Int)*big.Int{
-	base := big.NewInt(1)
-	for _,i := range list{
-		base.Mul(base,i)
-	}
-	return base
-}
 
 //func(t *miniCC) issueCertificate(stub shim.ChaincodeStubInterface,args []string) peer.Response {
 //	stateAsBytes,err :=stub.GetState(ISSUESTATE)
